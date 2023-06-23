8f42eabb95dbedfa9d92538e15e9375266cd654b
NuttawanR <reabreang.nuttawan@gmail.com>
2023-06-23 13:46:48 +0700

    next.js + mui install

---
+++ resume-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import Page from './app-resume/page'
+
+export default function Home() {
+  return (
+    <main>
+      <p>Hello World</p>
+    </main>
+  )
+}
